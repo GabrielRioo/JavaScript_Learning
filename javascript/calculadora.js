f4183788c1331964111c6a1230a2c5808fe81665
GabrielRioo <Gabrielpaivario@gmail.com>
2020-07-13 22:31:51 -0300

    Calculadora em Javascript

---
+++ javascript/calculadora.js
@@ -0,0 +1,18 @@
+function calcular() {
+    let val1 = parseInt(document.getElementById("val1").value);
+    let val2 = parseInt(document.getElementById("val2").value);
+    let sinal = document.getElementById("sinal").value;
+    let result = document.getElementById("result");
+
+    console.log("valor 1: ", val1)
+    console.log("valor 2: ", val2)
+
+    if (sinal == "+") result.value = val1 + val2;
+    if (sinal == "-") result.value = val1 - val2;
+    if (sinal == "*") result.value = val1 * val2;
+    if (sinal == "/") result.value = val1 / val2;
+
+    console.log(val1, " + ", val2, " = ", result)
+
+
+}
---
+++ python/calculadora.py
@@ -0,0 +1,7 @@
+# Calculadora Simples:
+
+num1 = 0
+num2 = 0
+resultado = 0
+operacao = ''
+
