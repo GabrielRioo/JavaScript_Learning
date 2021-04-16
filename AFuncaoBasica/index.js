9cabbb54d262a860129a8580b59ea3463c296db5
GabrielRioo <gabrielpaivario@gmail.com>
2021-04-16 10:16:46 -0300

    add an event when click on the button to generate new message box

--- AFuncaoBasica/index.js
+++ AFuncaoBasica/index.js
@@ -1,23 +1,25 @@
+var btn = document.querySelector('button');
+btn.onclick = displayMessage;
+
 function displayMessage() {
     var html = document.querySelector('html');
-
+    
     // criar um elemento 'div' para uma caixa de mensagem.
     var panel = document.createElement('div');
     panel.setAttribute('class', 'msgBox'); // adicionar uma classe a tag div
     html.appendChild(panel); //identifica onde o panel(filho) irá aparecer na pagina
-
+    
     var msg = document.createElement('p');
     msg.textContent = 'This is a message box';
     panel.appendChild(msg);
-
+    
     var closeBtn = document.createElement('button');
     closeBtn.textContent = 'x';
+    closeBtn.style.color = 'red';
     panel.appendChild(closeBtn);
-
+    
     // quando clicar no 'X', vai remover a div 
     closeBtn.onclick = function() {
         panel.parentNode.removeChild(panel); //remove um elemento filho (panel)
     }
 }
-
-displayMessage();
