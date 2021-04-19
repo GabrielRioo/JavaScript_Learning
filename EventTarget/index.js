0ea472c830f5048460278e4b4cd0ab59469010d1
GabrielRioo <gabrielpaivario@gmail.com>
2021-04-19 09:13:14 -0300

    add an event on click to change the color of the divs

---
+++ EventTarget/index.js
@@ -0,0 +1,21 @@
+for (let i = 1; i <= 16; i++) {
+    const myDiv = document.createElement('div'); // cria um elemento div
+    document.body.appendChild(myDiv); 
+}
+
+function random(number) { // criar uma função para gerar numeros aleatorios
+    return Math.floor(Math.random() * number);
+}
+
+function bgChange() {
+    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    return rndCol;
+}
+
+const divs = document.querySelectorAll('div');
+
+for (let i = 0; i < divs.length; i++) {
+    divs[i].onclick = function(e) {
+        e.target.style.backgroundColor = bgChange();
+    }
+}
--- MudaCorAleatoria/index.js
+++ MudaCorAleatoria/index.js
@@ -4,9 +4,11 @@ function random(number) {
     return Math.floor(Math.random()*number);
 }
 
-function bgChange() {
+function bgChange(event) {
     var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-    document.body.style.backgroundColor = rndCol;
+    event.target.style.backgroundColor = rndCol;
+
+    console.log(event);
 }
 
 btn.addEventListener('mouseover', bgChange);
